6727221888c52e5f88b3cf2109e7912d6b0bb2c9
Guram Jajanidze <128817115+Gurijajo@users.noreply.github.com>
2024-10-31 09:58:39 -0700

    Last

--- public/js/script.js
+++ public/js/script.js
@@ -44,23 +44,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
 const scrollers = document.querySelectorAll(".scroller");
 
-// If a user hasn't opted in for recuded motion, then we add the animation
+
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
   addAnimation();
 }
 
 function addAnimation() {
   scrollers.forEach((scroller) => {
-    // add data-animated="true" to every `.scroller` on the page
     scroller.setAttribute("data-animated", true);
 
-    // Make an array from the elements within `.scroller-inner`
+    
     const scrollerInner = scroller.querySelector(".scroller__inner");
     const scrollerContent = Array.from(scrollerInner.children);
 
-    // For each item in the array, clone it
-    // add aria-hidden to it
-    // add it into the `.scroller-inner`
+    
     scrollerContent.forEach((item) => {
       const duplicatedItem = item.cloneNode(true);
       duplicatedItem.setAttribute("aria-hidden", true);
@@ -88,10 +85,10 @@ document.addEventListener("DOMContentLoaded", function() {
       let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
       if (scrollTop > lastScrollTop) {
-          // Downscroll
+          
           navbar.style.top = "-70px"; // Adjust based on your navbar height
       } else {
-          // Upscroll
+         
           navbar.style.top = "0";
       }
       lastScrollTop = scrollTop;
